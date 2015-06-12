5c29657be5aa740d92743a59136a9314b9486f33
hch12907 <haochengho12907@gmail.com>
2015-06-13 01:10:38 +0800

    Fixed some typo, added more method and comments.

--- Text-Based Game/GameCore/PlayerHandler/Inventory.cs
+++ Text-Based Game/GameCore/PlayerHandler/Inventory.cs
@@ -51,9 +51,9 @@ namespace Text_Based_Game.GameCore.PlayerHandler
                 {
                     if (ItemAmount[i] >= Reference.ItemStackAmount) //if the item amount exceeds the max, create a new slot...
                     {
-                        for (int i_2 = 0; i < Reference.InventoryTotalSlot; i++) //obviously loop.
+                        for (int i_2 = 0; i_2 < Reference.InventoryTotalSlot; i_2++) //obviously loop.
                         {
-                            if (ItemName[i_2] == "" || ItemName == null) //if the slot is empty, put the item into it
+                            if (ItemName == null) //if the slot is empty, put the item into it
                             {
                                 ItemName[i_2] = itemName;
                                 break;
@@ -65,9 +65,9 @@ namespace Text_Based_Game.GameCore.PlayerHandler
                     {
                         if ((ItemAmount[i] += amount) > Reference.ItemStackAmount) //checks if the "after-adding-amount" exceeds the max amount.
                         {
-                            for (int i_3 = 0; i < Reference.InventoryTotalSlot; i++) //obviously loop.
+                            for (int i_3 = 0; i_3 < Reference.InventoryTotalSlot; i_3++) //obviously loop.
                             {
-                                if (ItemName[i_3] == "" || ItemName == null) //if the slot is empty, put the item into it
+                                if (ItemName == null) //if the slot is empty, put the item into it
                                 {
                                     ItemName[i_3] = itemName;
                                     break;
@@ -84,13 +84,55 @@ namespace Text_Based_Game.GameCore.PlayerHandler
                 }
                 else
                 {
-                    if(ItemName[i] == "" || ItemName == null)
+                    if(ItemName == null)
                     {
                         ItemName[i] = itemName;
                         break;
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// Removes specified amount of a specified item.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="amount"></param>
+        public static void removeItem(string item, int amount)
+        {
+            for(int i = 0; i < Reference.InventoryTotalSlot; i++)
+            {
+                if(ItemName[i] == item)
+                {
+                    if ((ItemAmount[i] -= amount) >= 0)
+                    {
+                        ItemAmount[i] -= amount;
+                    }
+                    else
+                    {
+                        ItemName[i] = null;
+                        ItemAmount[i] = 0;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of the items' name as array.
+        /// </summary>
+        /// <returns></returns>
+        public static string[] ItemNameArray()
+        {
+            return ItemName;
+        }
+
+        /// <summary>
+        /// Returns the list of the items' name as array.
+        /// </summary>
+        /// <returns></returns>
+        public static int[] ItemAmountArray()
+        {
+            return ItemAmount;
         }
     }
 }
