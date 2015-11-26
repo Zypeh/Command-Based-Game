3b9a8777af4435de5ec83e7ae6ddef5376969d43
hch12907 <haochengho12907@gmail.com>
2015-11-26 12:27:52 +0800

    Continue adding more functions to struct Item

--- GameLogic/InventoryHandler/Item.h
+++ GameLogic/InventoryHandler/Item.h
@@ -2,11 +2,22 @@ struct Item
 {
     string item_name; // Item name
     int maximum_stack; // Items per stack
+    unsigned int current_amount; // Current amount player owns
 
     bool CompareName(Item item)
     {
         // this one should be set during item declaration!
         return (item_name == item.item_name);
+    }
+
+    bool IsStackFull()
+    {
+        return (current_amount >= maximum_stack)
+    }
+
+    void setCurrentAmount(int amount)
+    {
+        current_amount += amount;
     }
 };
 //EXAMPLES OF DECLARING ITEMS
