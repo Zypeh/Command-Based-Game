3d1886bdec5338c58377569fc3849a35a2f743c7
hch12907 <haochengho12907@gmail.com>
2015-12-04 16:13:48 +0800

    Basic functionality of world generation.

--- GameplayLogic/WorldHandler/WorldGen.cpp
+++ GameplayLogic/WorldHandler/WorldGen.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
-#include <random>
-#include <functional>
 
 using namespace std;
 
-void WorldGen()
-{
+#include "../../common/randomizer.h"
 
+int grass;
+int tree;
+int cow;
+int chicken;
+int pig;
+int sheep;
+bool nearCave;
+
+void generateWorld()
+{
+    grass = randomInt(5, 25);
+    tree = randomInt(10, 20);
+    cow = randomInt(0, 10);
+    chicken = randomInt(0, 10);
+    pig = randomInt(0, 10);
+    sheep = randomInt(0, 10);
+    nearCave = randomBool();
+}
+
+int returnWorldInfo(string type)
+{
+    if(type == "grass") { return grass; }
+    else if(type == "tree") { return tree; }
+    else if(type == "cow") { return cow; }
+    else if (type == "chicken") { return chicken; }
+    else if (type == "pig") { return pig; }
+    else if (type == "sheep") { return sheep; }
+    else { return -1;}
 }
--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 69;
-	static const long REVISION  = 2241;
+	static const long BUILD  = 72;
+	static const long REVISION  = 2264;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 341;
-	#define RC_FILEVERSION 0,1,69,2241
-	#define RC_FILEVERSION_STRING "0, 1, 69, 2241\0"
-	static const char FULLVERSION_STRING [] = "0.1.69.2241";
+	static const long BUILDS_COUNT  = 344;
+	#define RC_FILEVERSION 0,1,72,2264
+	#define RC_FILEVERSION_STRING "0, 1, 72, 2264\0"
+	static const char FULLVERSION_STRING [] = "0.1.72.2264";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 70;
+	static const long BUILD_HISTORY  = 73;
 	
 
 }
