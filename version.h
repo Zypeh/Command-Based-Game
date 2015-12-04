fa34496b9b00516eba5a60cf495a7386f88cfa8e
hch12907 <haochengho12907@gmail.com>
2015-12-04 16:17:58 +0800

    Moved PlayerStatus.cpp and header to a folder.

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 72;
-	static const long REVISION  = 2264;
+	static const long BUILD  = 73;
+	static const long REVISION  = 2286;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 344;
-	#define RC_FILEVERSION 0,1,72,2264
-	#define RC_FILEVERSION_STRING "0, 1, 72, 2264\0"
-	static const char FULLVERSION_STRING [] = "0.1.72.2264";
+	static const long BUILDS_COUNT  = 345;
+	#define RC_FILEVERSION 0,1,73,2286
+	#define RC_FILEVERSION_STRING "0, 1, 73, 2286\0"
+	static const char FULLVERSION_STRING [] = "0.1.73.2286";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 73;
+	static const long BUILD_HISTORY  = 74;
 	
 
 }
