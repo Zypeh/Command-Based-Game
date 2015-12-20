2102089db4f044506fbdce445f5bc48157c0ebde
hch12907 <haochengho12907@gmail.com>
2015-12-20 12:05:52 +0800

    ItemProperty.

--- GameLogic/InventoryHandler/Item.h
+++ GameLogic/InventoryHandler/Item.h
@@ -1,15 +1,21 @@
 #ifndef ITEM
 #define ITEM
 
+#include <map>
+struct ItemProperty
+{
+    std::string property_name;
+    std::string property_description;
+    std::map<std::string, int> property_info;
+};
+
 struct Item
 {
     std::string item_name = ""; // Item name
-    unsigned int maximum_stack = 0; // Items per stack
-    unsigned int current_amount = 0; // Current amount player owns
     int type = 0;
-    int item_damage;
-    int item_attackdamage;
-
+    ItemProperty properties[];
+    unsigned int maximum_stack = 0; // Maximum items per stack
+    unsigned int current_amount = 0; // Current amount player owns
     bool CompareName(Item item)
     {
         // this one should be set during item declaration!
@@ -21,11 +27,13 @@ struct Item
         return (current_amount >= maximum_stack);
     }
 
-    void setItem(std::string name, int amnt, int stck)
+    void setItem(std::string name, int amnt, int stck, ItemProperty property[])
     {
         item_name = name;
         current_amount = amnt;
         maximum_stack = stck;
+   //     properties = property;
     }
 };
+
 #endif
